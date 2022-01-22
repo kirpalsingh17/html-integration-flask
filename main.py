1e96ff5f2999361c6a4b9ad9d02021e72fbcf37b
Kirpal Singh <mailme.kirpal@gmail.com>
2022-01-22 16:38:56 +0530

    Config json composition

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import json
 
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html',params=params)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',params=params)
 
 @app.route("/project")
 def contact():
-    return render_template('project.html')
+    return render_template('project.html', params=params)
 
 
 app.run(debug=True)
